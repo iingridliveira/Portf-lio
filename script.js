2e889af1ec3bbdafef7092bd08243e540a26fcff
Ingrid <oligrid20@gmail.com>
2023-12-21 19:25:36 -0300

    botão do dia

--- script.js
+++ script.js
@@ -36,3 +36,12 @@ class MobileNavbar{
 
 const mobileNavbar = new MobileNavbar(".mobile-menu",".nav-list",".nav-list li");
 mobileNavbar.init();
+function togleModel(){
+  const html= document.documentElement
+  html.classList.toggle("light")
+  const img= document.querySelector("#profile img")
+  img = (html.classList.contains('light')) 
+  ? img.setAttribute('src',"assets/avatar-light.png") 
+  : img.setAttribute('src',"assets/avatar.png");
+  }
+ 
